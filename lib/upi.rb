9a6e943ec32efe7c99d3f85e12da5637cd998cc6
Faraz Noor <faraznoor75@gmail.com>
2024-09-05 23:35:48 +0530

    published Gem

--- lib/upi.rb
+++ lib/upi.rb
@@ -5,17 +5,27 @@ require_relative "upi/version"
 
 module Upi
   class Generator
-    def initialize(amount, upi_id, name, description = '', currency = 'INR')
-      @amount = amount
-      @upi_id = upi_id
-      @name = name
-      @description = description
-      @currency = currency
+    attr_reader :params
+
+    def initialize(upi_id:, name:, amount: nil, currency: 'INR', note: '', merchant_code: nil, transaction_ref_id: nil, transaction_id: nil, url: nil)
+      @params = {
+        pa: upi_id,
+        pn: name,
+        am: amount,
+        cu: currency,
+        tn: note,
+        mc: merchant_code,
+        tr: transaction_ref_id,
+        tid: transaction_id,
+        url: url
+      }.compact
     end
 
     def generate_qr
       content = upi_content
       qrcode = RQRCode::QRCode.new(content)
+
+      # Output SVG format QR code
       qrcode.as_svg(
         color: '000',
         shape_rendering: 'crispEdges',
@@ -26,7 +36,9 @@ module Upi
     private
 
     def upi_content
-      "upi://pay?pa=#{@upi_id}&pn=#{@name}&am=#{@amount}&cu=#{@currency}&tn=#{@description}"
+      # Manually construct the UPI URI string without URI::UPI
+      query_string = URI.encode_www_form(params)
+      "upi://pay?#{query_string}"
     end
   end
 
--- spec/upi_spec.rb
+++ spec/upi_spec.rb
@@ -1,8 +1,97 @@
 require 'upi'
 
 RSpec.describe Upi::Generator do
-  it 'generates a UPI QR code' do
-    generator = Upi::Generator.new(100, 'test@upi', 'Test Name', 'Test Description')
-    expect(generator.generate_qr).to include('svg')
+  context 'Individual QR Code' do
+    let(:individual_params) do
+      {
+        upi_id: 'test@upi',
+        name: 'Test User',
+        amount: 100,
+        note: 'Personal Payment'
+      }
+    end
+
+    describe '#initialize' do
+      it 'initializes with correct attributes' do
+        generator = Upi::Generator.new(**individual_params)
+
+        expect(generator.params[:pa]).to eq('test@upi')
+        expect(generator.params[:pn]).to eq('Test User')
+        expect(generator.params[:am]).to eq(100)
+        expect(generator.params[:cu]).to eq('INR')
+        expect(generator.params[:tn]).to eq('Personal Payment')
+      end
+    end
+
+    describe '#upi_content' do
+      it 'generates the correct UPI content for an individual' do
+        generator = Upi::Generator.new(**individual_params)
+        upi_content = generator.send(:upi_content)
+
+        expect(upi_content).to eq('upi://pay?pa=test%40upi&pn=Test+User&am=100&cu=INR&tn=Personal+Payment')
+      end
+    end
+
+    describe '#generate_qr' do
+      it 'generates a valid QR code in SVG format for an individual' do
+        generator = Upi::Generator.new(**individual_params)
+        qr_code_svg = generator.generate_qr
+
+        expect(qr_code_svg).to include('<svg')
+        expect(qr_code_svg).to include('xmlns="http://www.w3.org/2000/svg"')
+      end
+    end
+  end
+
+  context 'Merchant QR Code' do
+    let(:merchant_params) do
+      {
+        upi_id: 'merchant@upi',
+        name: 'Merchant Name',
+        amount: 500,
+        currency: 'INR',
+        note: 'Payment for Goods',
+        merchant_code: '1234',
+        transaction_ref_id: 'REF123',
+        transaction_id: 'TXN456',
+        url: 'https://merchant.com/payment'
+      }
+    end
+
+    describe '#initialize' do
+      it 'initializes with correct merchant-specific attributes' do
+        generator = Upi::Generator.new(**merchant_params)
+
+        expect(generator.params[:pa]).to eq('merchant@upi')
+        expect(generator.params[:pn]).to eq('Merchant Name')
+        expect(generator.params[:am]).to eq(500)
+        expect(generator.params[:cu]).to eq('INR')
+        expect(generator.params[:tn]).to eq('Payment for Goods')
+        expect(generator.params[:mc]).to eq('1234')
+        expect(generator.params[:tr]).to eq('REF123')
+        expect(generator.params[:tid]).to eq('TXN456')
+        expect(generator.params[:url]).to eq('https://merchant.com/payment')
+      end
+    end
+
+    describe '#upi_content' do
+      it 'generates the correct UPI content for a merchant' do
+        generator = Upi::Generator.new(**merchant_params)
+        upi_content = generator.send(:upi_content)
+        expect(upi_content).to eq(
+                                 'upi://pay?pa=merchant%40upi&pn=Merchant+Name&am=500&cu=INR&tn=Payment+for+Goods&mc=1234&tr=REF123&tid=TXN456&url=https%3A%2F%2Fmerchant.com%2Fpayment'
+                               )
+      end
+    end
+
+    describe '#generate_qr' do
+      it 'generates a valid QR code in SVG format for a merchant' do
+        generator = Upi::Generator.new(**merchant_params)
+        qr_code_svg = generator.generate_qr
+
+        expect(qr_code_svg).to include('<svg')
+        expect(qr_code_svg).to include('xmlns="http://www.w3.org/2000/svg"')
+      end
+    end
   end
 end
